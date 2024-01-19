2f5eafe552cf4967a6f03f0ca28c311f1707be1b
“zsj” <shijie_go@163.com>
2024-01-19 08:39:04 +0800

    更新base_path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'JavaScript常用操作',
   description: '写写博客，涨涨见识',
-  base: '/write-blog/',
+  base: '/mimosa-blog/docs/.vuepress/dist',
   theme: 'reco',
   themeConfig: {
     nav: [
